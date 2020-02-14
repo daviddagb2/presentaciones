d0a4c661decf6e66086acfcbe02b691ee8cce49f
David Gonzalez <daviddagb2@gmail.com>
2020-02-14 16:27:34 -0600

    agregado if check exec

--- pull.php
+++ pull.php
@@ -1,7 +1,12 @@
 <?php
 
-// Use in the “Post-Receive URLs” section of your GitHub repo.
+if(function_exists('shell_exec')) {
+    echo "exec is enabled";
+}else{
+	echo "exec NOT enabled";
+}
 
+// Use in the “Post-Receive URLs” section of your GitHub repo.
 if ( $_POST['payload'] ) {
 	shell_exec( "cd /usr/share/nginx/presentaciones/ && git reset --hard && git pull" );
 	echo "done";
