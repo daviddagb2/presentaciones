3ee0df27679d3a00885c60694c0c5ecc1862a443
David Gonzalez <daviddagb2@gmail.com>
2020-02-14 16:15:02 -0600

    Actualizado el pull.php

--- pull.php
+++ pull.php
@@ -3,7 +3,7 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-	shell_exec( "cd /user/share/nginx/presentaciones/ && git reset –hard HEAD && git pull" );
+	shell_exec( "cd /usr/share/nginx/presentaciones/ && git reset --hard && git pull" );
 }
 
 ?>
